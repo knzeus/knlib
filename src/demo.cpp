c23a4be1fc4db0cc21b4de715467f6919cc0f93a
zhoujie <knzeus@qq.com>
2015-04-10 15:37:07 +0800

    add demo

---
+++ src/demo.cpp
@@ -0,0 +1,39 @@
+/***************************************************************************
+ * 
+ * Copyright (c) 2015 Baidu.com, Inc. All Rights Reserved
+ * $Id$ 
+ * 
+ **************************************************************************/
+ 
+ 
+ 
+/**
+ * @file src/demo.cpp
+ * @author knzeus(zhou_jie@baidu.com)
+ * @date 2015/04/10 15:36:29
+ * @version $Revision$ 
+ * @brief 
+ *  
+ **/
+
+void foo() {}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* vim: set ts=4 sw=4 sts=4 tw=100 */
